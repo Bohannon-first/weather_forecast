362ff6bc7769b694b1336f123454d2a27d233c8c
Ярослав <spartacus215@yandex.ru>
2022-08-31 17:37:57 +0300

    Исправил баг с фильтрами, добавил анимацию удаления и появления карточек

--- js/filters.js
+++ js/filters.js
@@ -1,8 +1,9 @@
 import {listBigCardsWeather} from './favorites-cities.js';
 
-const listFilters = document.querySelector('.sort-form__group--filters');
+const filtersContainer = document.querySelector('.sort-form__group--filters');
+const btnDisableFilters = document.querySelector('#all-cities').parentElement;
 
-// Фильтры погоды
+// Отфильтровать города по погоде
 const onListFiltersClick = (evt) => {
   const favouriteCitiesList = listBigCardsWeather.querySelectorAll('.big-card');
   const currentFilter = evt.target.closest('.sort-form__input-wrapper');
@@ -14,13 +15,40 @@ const onListFiltersClick = (evt) => {
   if (currentFilterInput.checked) {
     favouriteCitiesList.forEach((city) => {
       const iconWeather = city.querySelector(`.icon--${currentFilterValue}`);
+
+      if (city.classList.contains('hidden-block')) {
+        city.classList.add('visible-block');
+        setTimeout(() => {
+          city.classList.remove('hidden-block');
+
+          city.classList.remove('big-card--animation-remove');
+          city.classList.remove('visible-block');
+        }, 301);
+      }
       if (!iconWeather) {
+        city.classList.add('big-card--animation-remove');
         setTimeout(() => {
           city.classList.toggle('hidden-block');
-        }, 300);
+          city.classList.remove('big-card--animation-remove');
+        }, 301);
       }
     });
   }
 };
 
-listFilters.addEventListener('click', onListFiltersClick);
+filtersContainer.addEventListener('click', onListFiltersClick);
+
+// Показать все города
+const onBtnDisableFiltersClick = () => {
+  const favouriteCitiesList = listBigCardsWeather.querySelectorAll('.big-card');
+  favouriteCitiesList.forEach((item) => {
+    item.classList.add('visible-block');
+    item.classList.remove('.big-card--animation-remove');
+    setTimeout(() => {
+      item.classList.remove('hidden-block');
+      item.classList.remove('visible-block');
+    }, 301);
+  });
+};
+
+btnDisableFilters.addEventListener('click', onBtnDisableFiltersClick);
